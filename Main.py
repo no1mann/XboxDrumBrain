27ea5296af15fceb645eb5d72d1d5a8a1050cbf2
No1mann <no1trevormann@gmail.com>
2018-08-21 02:08:01 -0400

    Full Drum Recognition

--- Main.py
+++ Main.py
@@ -6,7 +6,6 @@ clock = pygame.time.Clock()
 joysticks = []
 drum_joystick = None
 
-
 # Load all joysticks
 print "Listing all joysticks..."
 for i in range(0, pygame.joystick.get_count()):
@@ -16,7 +15,7 @@ for i in range(0, pygame.joystick.get_count()):
 
 print ""
 
-# Find drum joy stick
+# Find drum joystick
 for joy in joysticks:
     if "drum" in joy.get_name().lower():
         print "Drum Kit Found... (%s)" % (joy.get_name())
@@ -32,7 +31,26 @@ if drum_joystick is None:
 # Main drum listening loop
 while 1:
     clock.tick(100)
+    list = []
+    # Load button inputs
     for event in pygame.event.get():
         if event.type == pygame.JOYBUTTONDOWN:
-            if event.button == 0:
-                print "Yep"
+            list.append(event.button+1)
+
+    # Drum Buttons
+    if 2 in list and 10 in list:
+        print "Red Pad"
+    if 4 in list and 6 in list:
+        print "Yellow Cym"
+    if 4 in list and 10 in list:
+        print "Yellow Pad"
+    if 3 in list and 6 in list:
+        print "Blue Cym"
+    if 3 in list and 10 in list:
+        print "Blue Pad"
+    if 1 in list and 6 in list:
+        print "Green Cym"
+    if 1 in list and 10 in list:
+        print "Green Pad"
+    if 5 in list:
+        print "Kick"
